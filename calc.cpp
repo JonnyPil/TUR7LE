de7a5afe461600175e6c423eeb84267d8c46ea2e
JonnyPil <129272661+JonnyPil@users.noreply.github.com>
2023-10-11 18:56:55 +0100

    Create calc.cpp

---
+++ calc.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double num1, num2;
+    char operation;
+
+    // Get input from the user
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter an operator (+, -, *, /): ";
+    cin >> operation;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    // Perform the calculation based on the operator
+    switch (operation) {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+            break;
+        case '/':
+            if (num2 != 0) {
+                cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+            } else {
+                cout << "Error: Cannot divide by zero." << endl;
+            }
+            break;
+        default:
+            cout << "Error: Invalid operator." << endl;
+            break;
+    }
+
+    return 0;
+}
